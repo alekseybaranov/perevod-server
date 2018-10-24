d2729258a856ba3e2e961175f65189f10cdcb41c
Aleksey Baranov <albaranov@live.ru>
2018-10-25 01:48:11 +0300

    front+back

---
+++ front/precache-manifest.d280c27c96f057e5604c599e3b40520d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9c6f546ce925942ac610",
+    "url": "/static/js/main.9c6f546c.chunk.js"
+  },
+  {
+    "revision": "04ac3bcf81d3c33ad62d",
+    "url": "/static/js/1.04ac3bcf.chunk.js"
+  },
+  {
+    "revision": "4ff51260a06044cd59a64e26f09ace38",
+    "url": "/index.html"
+  }
+];
