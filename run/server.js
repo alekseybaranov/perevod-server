b98d6edfcbb83f9b65fb88d3abddd787473cbf79
Aleksey Baranov <albaranov@live.ru>
2018-10-25 00:38:05 +0300

    Исправлен server.js

--- run/server.js
+++ run/server.js
@@ -1,24 +1,41 @@
 // ----------------------------------------------------------------------------
 // Системные модули
 //
+var debug = require('debug')('server:server');
+var http = require('http');
 
 
 // ----------------------------------------------------------------------------
 // Собственные модули
 //
+const app = require('./app')
 
 // ----------------------------------------------------------------------------
 
-const app = require('./app')
+/**
+ * Получаем порт из переменной окружения и сохраняем в Express.
+ */
 
-let port = normalizePort(process.env.PORT || '3001')
-app.set('port', port)                               // порт по умолчанию - 3001
+var port = normalizePort(process.env.PORT || '3001'); // порт по умолчанию 3001
+app.set('port', port);
 
-app.listen(app.get('port'), function() {
-  console.log('\nСервер Express запущен в режиме ' + app.get('env') +
-              '\nпо адресу http://localhost:' + app.get('port') +
-              '\nдля завершения работы сервера нажните Ctrl+C.')
-})
+/**
+ * Создаём HTTP-сервер.
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Слушаем предоставленный порт на всех сетевых интерфейсах.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+console.log('\nСервер Express запущен в режиме ' + app.get('env') +
+            '\nпо адресу http://localhost:' + app.get('port') +
+            '\nдля завершения работы сервера нажните Ctrl+C\n')
 
 // ----------------------------------------------------------------------------
 // Функции
@@ -42,4 +59,44 @@ function normalizePort(val) {
   }
 
   return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
 }
